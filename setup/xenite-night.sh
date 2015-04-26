c590dd32422963e93d1b61b2cee45856677db5c9
Defman21 <thefyro1@gmail.com>
2015-04-26 18:50:03 +0300

    Updated installation scripts

--- setup/xenite-night.sh
+++ setup/xenite-night.sh
@@ -7,7 +7,7 @@ hash gtk2-engines-pixbuf 2>/dev/null || { sudo apt-get install gtk2-engines-pixb
 hash git 2>/dev/null || { sudo apt-get install git; }
  
 # Pull night source code to /usr/share/themes
-cd /usr/share/themes
-[ -d "/usr/share/themes/Xenite" ] && { cd /usr/share/themes/Xenite; sudo git pull origin master; } || { sudo git clone https://github.com/eOS-themes/Xenite.git; }
+cd /usr/share/themes/
+[ -d "/usr/share/themes/Xenite" ] && { cd ./Xenite; sudo git pull origin master; } || { sudo git clone https://github.com/eOS-themes/Xenite.git; }
 
 echo "Done!"
--- setup/xenite-stable.sh
+++ setup/xenite-stable.sh
@@ -2,17 +2,18 @@
 # Automated Install Script for Xenite theme - stable
 # Copyright (C) vinctux (Jaka Smrekar), 2015
 
+RELEASE = 0.1
+
 # Install Xenite's dependecies
 hash gtk2-engines-pixbuf 2>/dev/null || { sudo apt-get install gtk2-engines-pixbuf; }
- 
+
+cd ~/
 # Download latest stable release from github.com
-wget https://github.com/eOS-themes/Xenite/releases/download/0.1/Xenite.tar
- 
+wget https://github.com/eOS-themes/Xenite/releases/download/$RELEASE/Xenite.tar
 # Extract to /usr/share/themes
-tar -xvf Xenite.tar
-sudo cp -fr ~/Xenite /usr/share/themes/Xenite
- 
+tar -xvf ~/Xenite.tar
+sudo mv ~/Xenite /usr/share/themes/
 # Remove temporary release download
-rm Xenite.tar
+rm ~/Xenite.tar
 
 echo "Done!"
