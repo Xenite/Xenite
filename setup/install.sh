1ea675fc791bba818ceb6ae747f1896b30862fb6
Jaka Smrekar <vinctux@outlook.com>
2015-04-26 22:17:03 +0200

    Fix plank.

--- setup/install.sh
+++ setup/install.sh
@@ -10,4 +10,9 @@ hash git 2>/dev/null || { sudo apt-get install git; }
 cd /usr/share/themes/
 [ -d "/usr/share/themes/Xenite" ] && { cd ./Xenite; sudo git pull origin master; } || { sudo git clone https://github.com/eOS-themes/Xenite.git; }
 
+# Fix plank
+cd /usr/share/themes/Xenite 
+mkdir /usr/share/plank/themes/Xenite
+sudo cp -rf ./plank/* /usr/share/plank/themes/Xenite/
+
 echo "Done!"
