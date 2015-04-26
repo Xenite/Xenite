0aa920326c2c115c226476dfa2b9bb4178776df1
Jaka Smrekar <vinctux@outlook.com>
2015-04-26 16:58:27 +0200

    Added setup scripts.

---
+++ setup/xenite-night.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Automated Install Script for Xenite theme - night
+# Copyright (C) vinctux (Jaka Smrekar), 2015
+
+# Install Xenite's dependecies
+hash gtk2-engines-pixbuf 2>/dev/null || { sudo apt-get install gtk2-engines-pixbuf; }
+hash git 2>/dev/null || { sudo apt-get install git; }
+ 
+# Pull night source code to /usr/share/themes
+cd /usr/share/themes
+[ -d "/usr/share/themes/Xenite" ] && { cd Xenite; git pull origin master; } || { git clone https://github.com/eOS-themes/Xenite.git Xenite; }
